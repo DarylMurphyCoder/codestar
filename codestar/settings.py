356c114f444722deda9da3fec155798e0a28cd86
DarylMurphyCoder <darylmurphy875@gmail.com>
2025-12-17 12:13:20 +0000

    Set DEBUG=True to troubleshoot admin login

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^x!ggw5^&+f)tz)5gj==j$umo1x2@78842gx0jd7wg+w9z_^49'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','.herokuapp.com',]
 
