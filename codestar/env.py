23cafe4a8c60b7f5ad7fda92e64b5fbce49ea08d
DarylMurphyCoder <darylmurphy875@gmail.com>
2025-12-17 15:18:53 +0000

    Remove env.py from version control

--- codestar/env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# These are development environment variables
-# For production on Heroku, set these via: heroku config:set KEY=value
-# For local development, use SQLite (don't set DATABASE_URL)
-
-os.environ.setdefault(
-    "SECRET_KEY", "django-insecure-dev-key-change-in-production")
-os.environ.setdefault("DEBUG", "True")
