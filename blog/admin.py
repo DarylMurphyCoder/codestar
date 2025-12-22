861b0dfa0677a26ea24131e9348647169edc11bb
DarylMurphyCoder <darylmurphy875@gmail.com>
2025-12-22 13:17:57 +0000

    Switch to basic Django admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
 
 @admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):
+class PostAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'slug', 'status')
     search_fields = ['title']
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
 
 
 # Register your models here.
