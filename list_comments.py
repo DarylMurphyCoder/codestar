042b49bcf6b3778a13a51e5a5c48bb772836d0c7
DarylMurphyCoder <darylmurphy875@gmail.com>
2025-12-29 13:50:47 +0000

    Add list comments script

---
+++ list_comments.py
@@ -0,0 +1,13 @@
+﻿import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar.settings')
+django.setup()
+
+from blog.models import Comment
+
+# List all comments
+comments = Comment.objects.all()
+print(f'Total comments: {comments.count()}')
+for comment in comments:
+    print(f'ID: {comment.id}, Post: {comment.post.title}, Author: {comment.author}, Approved: {comment.approved}')
