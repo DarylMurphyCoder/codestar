690be1a4d062b40759d0a9194bdea3d78e348cb2
DarylMurphyCoder <darylmurphy875@gmail.com>
2025-12-29 13:48:11 +0000

    Set comments to approved

---
+++ approve_comments.py
@@ -0,0 +1,12 @@
+﻿import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar.settings')
+django.setup()
+
+from blog.models import Comment
+
+# Approve all comments
+comments = Comment.objects.filter(approved=False)
+count = comments.update(approved=True)
+print(f'Approved {count} comments')
