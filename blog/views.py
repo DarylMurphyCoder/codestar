f784ec40e89c358fcfb223c3b4a338a5ac5ffda1
DarylMurphyCoder <darylmurphy875@gmail.com>
2025-12-30 10:35:45 +0000

    add comment create flow

--- blog/views.py
+++ blog/views.py
@@ -19,11 +19,23 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
 
     if request.method == "POST":
-        print("Received a POST request")
+        if not request.user.is_authenticated:
+            messages.add_message(request, messages.ERROR, "You must be logged in to comment.")
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
-    print("About to render template")
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, "Comment submitted and awaiting approval.")
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        else:
+            messages.add_message(request, messages.ERROR, "Please correct the errors below.")
 
     return render(
         request,
@@ -32,6 +44,7 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
 
